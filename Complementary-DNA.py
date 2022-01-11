02b1d65abe8ae0fc17d3d6a7855c72f2ebf23f71
balpa <44147280+balpa@users.noreply.github.com>
2022-01-11 15:25:15 +0300

    Create Complementary-DNA.py

---
+++ Complementary-DNA.py
@@ -0,0 +1,8 @@
+"""
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side.
+DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+"""
+def DNA_strand(dna):
+    dic = {"A":"T","G":"C","T":"A","C":"G"}
+    final = [dic[x] for x in dna]
+    return ''.join(final)
